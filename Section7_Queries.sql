53f02079aded455d416254503b72b0071ec54356
Snap96 <gomolemomogau977@gmail.com>
2026-05-16 11:19:18 +0200

    Add examples and explanations for network address data types in SQL

--- Section7_Queries.sql
+++ Section7_Queries.sql
@@ -674,3 +674,72 @@ ALTER TABLE table_json
 ALTER COLUMN docs TYPE JSONB;
 
 CREATE INDEX ON table_json USING GIN(docs jsonb_path_ops);
+
+-- Network Address Data Types
+-- #################################
+/*
+	1.	PostgreSQL offers data types to store IPv4, IPv6 and MAC address
+
+	2.	Network Address Types
+		Name							Storage size					Notes
+		-------							----------------				-------------------------
+		cidr							7 or 19 bytes					IPv4 and IPv6 networks
+		inet							7 or 19 bytes					IPv4 and IPv6 hosts and networks
+		macaddr							6 bytes							MAC addresses
+		macaddr8						8 bytes							MAC addresses (EUI-64 format)
+
+		It is better to use these types instead of plain text types to store network addresses,
+		Because these types offer input error checking and specialized operators and functions
+
+	3.	Special sorting mechanism
+
+		When sorting inet or cidr data types, IPv4 addresses will always sort before IPv6 addresses,
+		including IPv4 addresses encapsulated or mapped to IPv6 addresses
+
+	4.	These types are bundled with indexing support and advanced functions and operator support.
+	
+*/
+
+
+-- 1.	Lets build a sample table with an IP address for IPv4 and IPv6 network address type i.e inet
+
+CREATE TABLE table_netaddr(
+	id SERIAL Primary Key,
+	ip INET
+);
+
+-- 2. Lets insert some data
+INSERT INTO table_netaddr(ip) VALUES
+('4.35.221.243'),
+('4.152.207.126'),
+('4.152.207.238'),
+('4.249.111.162'),
+('12.1.223.132'),
+('12.8.192.60');
+
+-- 3. Lets view the data
+SELECT * FROM table_netaddr;
+
+-- 4. Lets analyze entries for /24 networks ip addresses
+-- set_masklen function:	set netmask length for inet value
+
+SELECT
+	ip,
+	set_masklen(ip, 24) as inet_24
+FROM table_netaddr;
+
+-- 5. Lets convert inet to cidr type
+SELECT
+	ip,
+	set_masklen(ip, 24) as inet_24,
+	set_masklen(ip::cidr, 24) as cidr_24
+FROM table_netaddr;
+
+-- 6. We can also analyze other network like /27 /28 too
+SELECT
+	ip,
+	set_masklen(ip, 24) as inet_24,
+	set_masklen(ip::cidr, 24) as cidr_24,
+	set_masklen(ip::cidr, 27) as cidr_27,
+	set_masklen(ip::cidr, 28) as cidr_28
+FROM table_netaddr;
